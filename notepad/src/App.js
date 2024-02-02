33efd4659921a77557a3778c043a42c72910b102
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-02 14:51:12 +0530

    Structure added

--- notepad/src/App.js
+++ notepad/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
-
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 function App() {
   return (
-    <div></div>
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
   );
 }
 
