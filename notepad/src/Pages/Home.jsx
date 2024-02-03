c74ab5527e0a488f12376e8f736de19c43301a06
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-03 13:36:48 +0530

    Operation bar

--- notepad/src/Pages/Home.jsx
+++ notepad/src/Pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
+import { Menu, MenuItem, Select, Input } from "semantic-ui-react";
 import axios from "axios";
-import HomeHeader from "../Components/Home/HomeHeader";
 
 const Home = () => {
+
+  const sortOptions = [
+    { key: "", value: "", text: "Select" },
+    { key: "asc", value: "asc", text: "Ascending Order" },
+    { key: "desc", value: "desc", text: "Descending Order" },
+  ];
+
+  const filterOptions = [
+    { key: "", value: "", text: "Select" },
+    { key: "Important", value: "Important", text: "Important Notes" },
+    { key: "Local", value: "Local", text: "Local Notes" },
+  ];
+
   const [data, setData] = useState([]);
   const [pages, setPages] = useState("");
 
@@ -33,8 +45,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <HomeHeader/>
+    <div className="homeOperation">
+      <Menu secondary>
+        <MenuItem>
+          <Select placeholder="Sort by title" options={sortOptions} />
+        </MenuItem>
+        <MenuItem>
+          <Input icon="search" placeholder="Search notes..." />
+        </MenuItem>
+        <MenuItem>
+          <Select placeholder="Filter by category" options={filterOptions} />
+        </MenuItem>
+      </Menu>
     </div>
   );
 };
