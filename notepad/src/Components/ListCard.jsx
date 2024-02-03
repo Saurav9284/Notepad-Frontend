8f2d8a2c858618b6f6118365aab73b44697eef19
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-03 20:47:54 +0530

    Issue Fixed

--- notepad/src/Components/ListCard.jsx
+++ notepad/src/Components/ListCard.jsx
@@ -1,32 +1,24 @@
-import React from "react";import {
-    CardMeta,
-    CardHeader,
-    CardDescription,
-    CardContent,
-    Button,
-    Card,
-  } from 'semantic-ui-react'
+// Inside ListCard.js
+import React from "react";
+import { Card, Button } from "semantic-ui-react";
 
 const ListCard = ({data}) => {
+  
   return (
     <Card>
-      <CardContent>
-        <CardHeader>{data.title}</CardHeader>
-        <CardMeta>{data.category}</CardMeta>
-        <CardDescription>
-          {data.description}
-        </CardDescription>
-      </CardContent>
-      <CardContent extra>
-        <div className='ui two buttons'>
-          <Button basic color='green'>
-            Edit
-          </Button>
-          <Button basic color='red'>
-            Delete
-          </Button>
-        </div>
-      </CardContent>
+      <Card.Content>
+        <Card.Header>{data.title}</Card.Header>
+        <Card.Meta>{data.category}</Card.Meta>
+        <Card.Description>{data.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button basic color="green" >
+          Edit
+        </Button>
+        <Button basic color="red" >
+          Delete
+        </Button>
+      </Card.Content>
     </Card>
   );
 };
